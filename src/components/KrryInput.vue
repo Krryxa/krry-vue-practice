9af98ed5866453c637fe48d7b300d03034b05ddf
Krryxa <krryxq@163.com>
2020-07-09 14:06:32 +0800

    feat: 自定义v-model

---
+++ src/components/KrryInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input :value="value" @change="$_handleChange" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    $_handleChange(e) {
+      this.$emit('input', e.target.value)
+    }
+  }
+}
+</script>
